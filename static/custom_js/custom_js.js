be1416f8e960cbed570ad496b00a43d87a2eb7e6
LaoHan <jimocheng97@hotmail.com>
2017-08-18 10:59:17 +0800

    update bug

--- static/custom_js/custom_js.js
+++ static/custom_js/custom_js.js
@@ -1,1 +1,22 @@
 w3.includeHTML();
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:true
+	        },
+	        600:{
+	            items:3,
+	            nav:false
+	        },
+	        1000:{
+	            items:3,
+	            nav:true,
+	            loop:false
+	        }
+	    }
+  });
+});
